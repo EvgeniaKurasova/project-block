75a4e43102382996e353b277ce692f264c6397cd
Evgenia <semenutenkoevgenia@gmail.com>
2023-06-21 22:58:42 +0300

    process contactTitle, contactChef and contactSubmit

---
+++ src/components/ContactSubmit/ContactSubmit.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import './ContactSubmit.scss'
+import React from 'react'
+import GreenLineDecor from 'components/GreenLineDecor/GreenLineDecor'
+
+interface Props {}
+
+const ContactSubmit = (props: Props) => {
+  return (
+    <div className="contactChefPart">
+        <h2 className='blockChehContacts'>Our Chef</h2>
+        <GreenLineDecor/>
+        <p className="contact-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  )
+}
+export default ContactSubmit
--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import Header2 from 'Conteiner/Header2/Header2'
+import ContactTitlePart from 'components/ContactTitlePart/ContactTitlePart'
+import ContactChefPart from 'components/ContactChefPart/ContactChefPart'
+import ContactSubmit from 'components/ContactSubmit/ContactSubmit'
+
 // import headerArrowDown from 'images/arrow_down.png'
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
@@ -8,6 +12,9 @@ const ContactsPage = (props: Props) => {
   return (
     <div>
       <Header2/>
+      <ContactTitlePart/>
+      <ContactChefPart/>
+      <ContactSubmit/>
     </div>
   )
 }
